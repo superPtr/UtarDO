cd5a7205e30e535c741de6227aa65f37d94d59c8
ptr <giantptrlifes@gmail.com>
2024-04-20 11:41:21 +0800

    resolving

--- app/src/main/java/my/edu/utar/utardo/splashScreen.java
+++ app/src/main/java/my/edu/utar/utardo/splashScreen.java
@@ -1,6 +1,8 @@
 package my.edu.utar.utardo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,13 @@ public class splashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                startActivity(new Intent(splashScreen.this, IntroActivity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
