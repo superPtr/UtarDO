7c0a581326333309019e578662588305749b8e59
ptr <giantptrlifes@gmail.com>
2024-04-20 11:35:07 +0800

    resolve

--- app/src/main/java/my/edu/utar/utardo/IntroActivity.java
+++ app/src/main/java/my/edu/utar/utardo/IntroActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
